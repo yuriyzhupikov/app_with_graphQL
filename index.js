dfb45dcef733ceb3990d3d3c3e86d82dbb5b1efc
Yurii <webdev56@gmail.com>
2022-02-13 19:21:47 +0300

    added: some types and recognizers

--- index.js
+++ index.js
@@ -1,25 +1,99 @@
 const {ApolloServer} = require('apollo-server');
 
 const typeDefs = `
+    enum PhotoCategory {
+        SELFIE
+        PORTRAIT
+        ACTION
+        LANDSCAPE
+        GRAPHIC
+    }
+    
+    input PostPhotoInput {
+        name: String!
+        description: String
+        category: PhotoCategory = PORTRAIT
+    }
+    type User {
+        githubLogin: ID!,
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+    }
+
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+        category: PhotoCategory!
+        postedBy: User!
+    }
+    
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
     }
     
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(input: PostPhotoInput!): Photo!
     }
 `;
+let _id = 0;
 
-let photos = [];
+var users = [
+    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+    { "githubLogin": "gPlake", "name": "Glen Plake" },
+    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+var photos = [
+    {
+        "id": "1",
+        "name": "Dropping the Heart Chute",
+        "description": "The heart chute is one of my favorite chutes",
+        "category": "ACTION",
+        "githubUser": "gPlake"
+    },
+    {
+        "id": "2",
+        "name": "Enjoying the sunshine",
+        "category": "SELFIE",
+        "githubUser": "sSchmidt"
+    },
+    {
+        id: "3",
+        "name": "Gunbarrel 25",
+        "description": "25 laps on gunbarrel today",
+        "category": "LANDSCAPE",
+        "githubUser": "sSchmidt"
+    }
+]
+
 const resolvers = {
     Query: {
         //Распознователь. Возвращает данные в типе и форме, заданных схемой
-        totalPhotos: () => 42
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
     Mutation: {
         postPhoto(parent, args) {
-            photos.push(args);
-            return true;
+            let newPhoto = {
+                id: _id++,
+                ...args.input
+            }
+            photos.push(newPhoto);
+            return newPhoto;
+        }
+    },
+    Photo: {
+        url:  parent => `http://yoursite.com/img/${parent.id}.jpg`,
+        postedBy: parent => {
+            return users.find(u => u.githubLogin === parent.githubUser);
+        }
+    },
+    User: {
+        postedPhotos: parent => {
+            return photos.filter(p => p.githubUser === parent.githubLogin);
         }
     }
 }
