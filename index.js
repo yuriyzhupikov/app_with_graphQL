70f9317c6dd4ceaf8aac4dc99d3328e18ee273d4
Yurii <webdev56@gmail.com>
2022-02-13 18:28:21 +0300

    added: type mutation - postPhoto

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ const typeDefs = `
     type Query {
         totalPhotos: Int!
     }
-`
+    
+    type Mutation {
+        postPhoto(name: String! description: String): Boolean!
+    }
+`;
+
+let photos = [];
 const resolvers = {
     Query: {
         //Распознователь. Возвращает данные в типе и форме, заданных схемой
         totalPhotos: () => 42
+    },
+    Mutation: {
+        postPhoto(parent, args) {
+            photos.push(args);
+            return true;
+        }
     }
 }
 
@@ -16,6 +28,7 @@ const server = new ApolloServer({
     typeDefs,
     resolvers
 });
+
 function startServer() {
     server
         .listen()
