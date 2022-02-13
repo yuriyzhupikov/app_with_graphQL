6d7f8588ae594c396de25c4c17bb3f67324ea6d4
Yurii <webdev56@gmail.com>
2022-02-13 20:57:43 +0300

    fixed: structure

--- index.js
+++ index.js
@@ -1,133 +1,15 @@
 const {ApolloServer} = require('apollo-server-express');
 const express = require('express');
+const {readFileSync} = require('fs');
+const expressPlayground = require('graphql-playground-middleware-express').default;
 const {GraphQLScalarType} = require('graphql');
 const config = require('config');
 
 const app = express();
 const PORT = config.get("port") || 4000;
 
-const typeDefs = `
-    enum PhotoCategory {
-        SELFIE
-        PORTRAIT
-        ACTION
-        LANDSCAPE
-        GRAPHIC
-    }
-    
-    input PostPhotoInput {
-        name: String!
-        description: String
-        category: PhotoCategory = PORTRAIT
-    }
-    type User {
-        githubLogin: ID!,
-        name: String
-        avatar: String
-        postedPhotos: [Photo!]!
-        inPhotos: [Photo!]!
-    }
-    
-    scalar DateTime
-    type Photo {
-        id: ID!
-        url: String!
-        name: String!
-        description: String
-        category: PhotoCategory!
-        postedBy: User!
-        taggedUsers: [User!]!
-        created: DateTime!
-    }
-    
-    type Query {
-        totalPhotos: Int!
-        allPhotos(after: DateTime): [Photo!]!
-    }
-    
-    type Mutation {
-        postPhoto(input: PostPhotoInput!): Photo!
-    }
-`;
-let _id = 0;
-
-const users = [
-    { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
-    { "githubLogin": "gPlake", "name": "Glen Plake" },
-    { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
-]
-const photos = [
-    {
-        "id": "1",
-        "name": "Dropping the Heart Chute",
-        "description": "The heart chute is one of my favorite chutes",
-        "category": "ACTION",
-        "githubUser": "gPlake",
-        "created": "3-28-1977"
-    },
-    {
-        "id": "2",
-        "name": "Enjoying the sunshine",
-        "category": "SELFIE",
-        "githubUser": "sSchmidt",
-        "created": "1-2-1985"
-    },
-    {
-        id: "3",
-        "name": "Gunbarrel 25",
-        "description": "25 laps on gunbarrel today",
-        "category": "LANDSCAPE",
-        "githubUser": "sSchmidt",
-        "created": "2018-04-15T19:09:57.308Z"
-    }
-]
-const tags = [
-    { "photoID": "1", "userID": "gPlake" },
-    { "photoID": "2", "userID": "sSchmidt" },
-    { "photoID": "2", "userID": "mHattrup" },
-    { "photoID": "2", "userID": "gPlake" }
-]
-
-const resolvers = {
-    Query: {
-        //Распознователь. Возвращает данные в типе и форме, заданных схемой
-        totalPhotos: () => photos.length,
-        allPhotos: () => photos
-    },
-    Mutation: {
-        postPhoto(parent, args) {
-            let newPhoto = {
-                id: _id++,
-                ...args.input,
-                created: new Date()
-            }
-            photos.push(newPhoto);
-            return newPhoto;
-        }
-    },
-    Photo: {
-        url:  parent => `http://yoursite.com/img/${parent.id}.jpg`,
-        postedBy: parent => users.find(u => u.githubLogin === parent.githubUser),
-        taggedUsers: parent => tags
-            .filter(tag => tag.photoID === parent.ID)
-            .map(tag => tag.userID)
-            .map(userID => users.find(u => u.githubLogin === userID))
-    },
-    User: {
-        postedPhotos: parent => photos.filter(p => p.githubUser === parent.githubLogin),
-        inPhotos: parent => tags
-            .filter(tag => tag.userID === parent.id)
-            .map(tag => tag.photoID)
-            .map(photoID => photos.find(p => p.id === photoID))
-    },
-    DateTime: new GraphQLScalarType({
-        name:'DateTime',
-        description: 'A valid date time value',
-        parseValue: value => new Date(value),
-        serialize: value => new Date(value).toISOString(),
-        parseLiteral: ast => ast.value
-    })
-}
+const typeDefs = readFileSync('schema/typeDefs.graphql', 'UTF-8');
+const resolvers = require('./resolvers');
 
 const server = new ApolloServer({
     typeDefs,
@@ -135,6 +17,7 @@ const server = new ApolloServer({
 });
 
 app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+app.get('/playground', expressPlayground({endpoint: '/typeDefs.graphql'}));
 
 async function startServer() {
     await server.start();
